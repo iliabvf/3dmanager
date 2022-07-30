5eb70e33a2b66045ee3f673ada48299fea1f7b74
Vasek <vasika@gmail.com>
2022-07-30 22:27:06 +0300

    Added Vaadin

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -12,7 +12,11 @@ import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.treegrid.TreeGrid;
 import com.vaadin.flow.router.PageTitle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The main view is a top-level placeholder for other views.
@@ -45,8 +49,10 @@ public class MainLayout extends AppLayout {
         H2 appName = new H2("3D Manager");
         appName.addClassNames("app-name");
 
-        com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName,
-                createNavigation(), createFooter());
+        com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName
+//                , createNavigation()
+                , createTree()
+                , createFooter());
         section.addClassNames("drawer-section");
         return section;
     }
@@ -58,6 +64,55 @@ public class MainLayout extends AppLayout {
         nav.addItem(new AppNavItem("Models", ModelsView.class, "la la-th-list"));
 
         return nav;
+    }
+
+    public class Person{
+        String firstName;
+        String lastName;
+        String email;
+
+        public Person(String firstName, String lastName, String email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
+
+    public List<Person> getStaff(Person manager) {
+        List<Person> managers = new ArrayList<>();
+        if (manager == null) {
+            managers.add(new Person("Interior", "", ""));
+            managers.add(new Person("Exterior", "", ""));
+        } else if (manager.getFirstName().equals("Interior")) {
+            managers.add(new Person("Tables", "", ""));
+            managers.add(new Person("Stairs", "", ""));
+        } else if (manager.getFirstName().equals("Exterior")) {
+            managers.add(new Person("Houses", "", ""));
+            managers.add(new Person("Trees", "", ""));
+        }
+        return managers;
+    }
+    private Component createTree(){
+
+        TreeGrid<Person> treeGrid = new TreeGrid<>();
+        treeGrid.setItems(getStaff(null), this::getStaff);
+        treeGrid.addHierarchyColumn(Person::getFirstName).setHeader("First name");
+//        treeGrid.addColumn(Person::getLastName).setHeader("Last name");
+//        treeGrid.addColumn(Person::getEmail).setHeader("Email");
+
+        return treeGrid;
     }
 
     private Footer createFooter() {
