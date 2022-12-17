2603df9b1618dd2c902fd8b905ae7d8f6e026d8a
vasek <vasika@gmail.com>
2022-12-17 10:07:41 -0500

    bug image

---
+++ start_server.bat
@@ -0,0 +1,7 @@
+SET MAVEN_HOME="C:\maven"
+SET MAVEN_OPTS="-Xms512M -Xmx6G -Xss2M -XX:MaxMetaspaceSize=1024M"
+SET MAVEN_OPTS="-Xms2G -Xmx16G"
+SET MAVEN_OPTS="-Xmx32G"
+
+mvn spring-boot:run
+pause 0
